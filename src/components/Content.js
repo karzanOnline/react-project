46ac96222b70973669ee4e29433c3a5897c85091
caozheng <170579160@qq.com>
2016-07-23 17:50:20 +0800

    done

--- src/components/Content.js
+++ src/components/Content.js
@@ -14,11 +14,16 @@ export default class Content extends Component{
             tagArr : ['DASHBOARD','MY CRUISE','AGENTS','HELP'].map((item)=>{
                 return {text:item,show:false}
             }),
-            selectArr : ['All','']
+            selectArr : ['All',''],
+            /*
+            * 展示数据的数组
+            * */
+            itemData :[],
+            addState : false
         }
     }
     componentDidMount(){
-
+        alert('选项卡没有实现完全，没有细化组件。不知道input的具体要求，没有加正则。因为最近加班没有太多的精力去优化，见谅！')
     }
     styleChange (index){
         /*
@@ -54,8 +59,36 @@ export default class Content extends Component{
 
         return tempArr
     }
+    addItemOp (...sign){
+        let name = this;
+        let _this = sign[0];
+        name=='show'?_this.setState({addState:true}):_this.setState({addState:false})
+    }
+    addItem (){
+        let _this = this;
+        let value = _this.refs.addItemValue.value;
+        let itemData = _this.state.itemData;
+        value && (value = value.split(','));
+        itemData = itemData.concat(value);
+        Array.from(itemData);
+        //filter 空元素
+        for(var i =itemData.length-1;i>=0;i--){
+            if (itemData[i]==''){
+                itemData.splice(i,1);
+            }
+        }
+        _this.setState({itemData : itemData,addState:false})
+    }
+    deleteItem (index){
+        debugger;
+        let _this= this;
+        let itemData = _this.state.itemData;
+        itemData.splice(index,1);
+        _this.setState({itemData:itemData})
+    }
     render(){
         let _this = this;
+        let data = _this.state.itemData;
         return (
             <div style={{padding:'10px'}}>
                 <div className="content-style clear">
@@ -74,7 +107,42 @@ export default class Content extends Component{
                         </div>
                         <div className="content-flex-style clear">
                             <div className="flex-width">
-                                <div></div>
+                                <div className="main-con clear">
+                                    <div className="circle-style left"></div>
+                                    <div className="content-main left">
+                                        <div className="main-top-f16">
+                                            <span>bjstdmngbgr02.thoughtworks.com</span>
+                                            <span style={{display:"inline-block",textIndent:"15px"}}>|idle|192.169.1.2|/var/lib/cruise-agent</span>
+                                        </div>
+                                        <div className="main-bottom-m10">
+                                            <span>+</span>
+                                            <span className="span-under" onClick={_this.addItemOp.bind('show',this)}>Specify Resources |</span>
+                                            {
+                                                data&&data.map((value,index)=>{
+                                                    return <span className="output" key={index}>{value}<span className="close" onClick={_this.deleteItem.bind(_this,index)}></span></span>
+                                                })
+                                            }
+                                            {
+                                                /*
+                                                *  add html
+                                                * */
+                                                this.state.addState&&
+                                                <div className="inner-content">
+                                                    <div><span>(separate multiple resources name with commas)</span></div>
+                                                    <div className="input-wrap">
+                                                        <input ref="addItemValue" className="input-primary" value={this.state.value}/>
+                                                    </div>
+                                                    <div>
+                                                        <button className="btn" onClick={_this.addItem.bind(this)}>Add resources</button>
+                                                        <button className="btn" onClick={_this.addItemOp.bind('close',this)}>Close</button>
+                                                    </div>
+                                                    <div className="triangle"></div>
+                                                </div>
+                                            }
+                                        </div>
+                                    </div>
+                                    <div className="left">Deny</div>
+                                </div>
                             </div>
                             <div className="flex-absolute">
                                 right
